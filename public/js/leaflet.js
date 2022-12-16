8d0435809a903e05bec519abb7ffc7c90a2b3bb7
Diego Moura <diegomoura.contact@gmail.com>
2022-12-16 00:58:35 -0300

    leaflet.js reorganizado

--- public/js/leaflet.js
+++ public/js/leaflet.js
@@ -1,54 +1,46 @@
-// Creating map options
-var mapOptions = {
-    center: [-23.0044222, -44.2880942],
-    zoom: 13
-}
- 
-// Creating a map object
-var map = new L.map('map', mapOptions);
+// Mapas
+    var mapOptions = {
+        center: [-23.0044222, -44.2880942],
+        zoom: 13
+    }
 
-// Creating a Layer object
-var googleMap = new L.TileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}');
+    var map = new L.map('map', mapOptions);
 
-var googleSat = new L.TileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
-    maxZoom: 20,
-    subdomains:['mt0','mt1','mt2','mt3']
-});
+    var googleMap = new L.TileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}');
 
-// Adding layer to the map
-googleSat.addTo(map);
+    var googleSat = new L.TileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+        maxZoom: 20,
+        subdomains:['mt0','mt1','mt2','mt3']
+    });
 
-// elementos
-var points = []
-var mark =[]
-var poligon
-var now
-
-function onMapClick(e) {
-    now = L.marker(e.latlng)//.addTo(map);
-    //now.push(points)
-    points.push(e.latlng);
-    //map.addLayer(points)
-    now.addTo(map);
-
-    map.removeLayer(poligon);
-    poligon = L.polygon(points).addTo(map);
-
-    
-}
+    googleSat.addTo(map);
 
 
-map.on('click', onMapClick);
+// Marcadores e Poligonos
+    var pontos = []
+    var markLayer
+    var polygonLayer
 
-// eventos
-document.getElementById("btei").addEventListener("click", tei);
-document.getElementById("bdel").addEventListener("click", del);
+    function onMapClick(e) {
+        markLayer = L.marker(e.latlng).addTo(map);
+        pontos.push(e.latlng);
+        
+        map.removeLayer(polygonLayer);
+        polygonLayer = L.polygon(pontos).addTo(map);
+        
+    }
 
+    function tei(){
+        polygonLayer = L.polygon(pontos).addTo(map);
+        
+    }
 
-function tei(){
-    poligon = L.polygon(points).addTo(map);
-    
-}
-function del(){
-    map.removeLayer(poligon);
-}
+    function del(){
+        map.removeLayer(polygonLayer);
+    }
+
+    // Eventos
+    map.on('click', onMapClick);
+
+    document.getElementById("btei").addEventListener("click", tei);
+    document.getElementById("bdel").addEventListener("click", del);
