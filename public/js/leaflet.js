2f48eb61543cfa0154d4072ca6f233c159746388
Diego Moura <diegomoura.contact@gmail.com>
2022-12-17 08:33:27 -0300

    Inclusao de poligonos funcional

--- public/js/leaflet.js
+++ public/js/leaflet.js
@@ -18,29 +18,68 @@
 
 // Marcadores e Poligonos
     var pontos = []
-    var markLayer
+    var markLayer = []
     var polygonLayer
+    var centralMarker
+
+    var nPoligono = false;
 
     function onMapClick(e) {
-        markLayer = L.marker(e.latlng).addTo(map);
-        pontos.push(e.latlng);
-        
-        map.removeLayer(polygonLayer);
-        polygonLayer = L.polygon(pontos).addTo(map);
+        if (nPoligono) {
+            pontos.push({"lat":e.latlng.lat, "lng":e.latlng.lng});
+            markLayer.push(L.marker({"lat":e.latlng.lat, "lng":e.latlng.lng}).addTo(map))
+
+            /*
+            if (centralMarker) map.removeLayer(centralMarker);
+            centralMarker = L.marker(pontoCent(pontos)).addTo(map);
+            */
+
+            if (pontos.length > 1) map.removeLayer(polygonLayer);
+            polygonLayer = L.polygon(pontos).addTo(map);
+            
+        }
         
     }
+    
 
-    function tei(){
-        polygonLayer = L.polygon(pontos).addTo(map);
+    function pontoCent(pts) {
+        let x = 0
+        let y = 0
+        let pCent = {
+            "lat": 0,
+            "lng": 0
+        }
+
+        pts.forEach(vertice => {
+            x += vertice.lat;
+            y += vertice.lng;
+        })
+        
+        pCent.lat = x/pts.length;
+        pCent.lng = y/pts.length;
+
+        return pCent;
+    }
+
+    function novoPoligono(){
+        nPoligono = !nPoligono;
         
     }
 
     function del(){
-        map.removeLayer(polygonLayer);
+        if (nPoligono) {
+            map.removeLayer(polygonLayer);
+            markLayer.forEach((marker, id) => {
+                map.removeLayer(markLayer[id]);
+                pontos.pop();//------------------------------
+            })
+
+        }
+        
     }
 
     // Eventos
     map.on('click', onMapClick);
 
-    document.getElementById("btei").addEventListener("click", tei);
+    document.getElementById("btei").addEventListener("click", novoPoligono);
     document.getElementById("bdel").addEventListener("click", del);
